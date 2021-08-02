ca58a9016e0abed92743687b55bbb4420775d9d4
xhnatiuk <maximilian.hnatiuk@gmail.com>
2021-08-02 09:47:46 -0700

    implement and test get_hwb, use mod for hsla

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,20 @@ pub struct Hsl {
 }
 
 #[wasm_bindgen]
+#[derive(Debug, PartialEq)]
+pub struct Hwb {
+    pub h: f32,
+    pub w: f32,
+    pub b: f32,
+    pub a: f32,
+}
+
+#[wasm_bindgen]
 pub fn get_hsl(string: &str) -> Option<Hsl> {
-    let re = Regex::new(r"^hsla?\(\s*([+-]?(?:\d{0,3}\.)?\d+)(?:deg)?\s*,?\s*([+-]?[\d\.]+)%\s*,?\s*([+-]?[\d\.]+)%\s*(?:[,|/]\s*([+-]?[\d\.]+)\s*)?\)$").unwrap();
+    let re = Regex::new(r"^hsla?\(\s*([+-]?\d{0,3}(?:\.\d+)?)(?:deg)?\s*,?\s*([+-]?[\d\.]+)%\s*,?\s*([+-]?[\d\.]+)%\s*(?:[,|/]\s*([+-]?[\d\.]+)\s*)?\)$").unwrap();
     let captures = re.captures(string)?;
     let h: f32 = match captures.get(1)?.as_str().parse::<f32>() {
-        Ok(num) => clamp(num, 0.0, 360.0),
+        Ok(num) => num.rem_euclid(360.0),
         Err(_e) => return None,
     };
     let s: f32 = match captures.get(2)?.as_str().parse::<f32>() {
@@ -40,6 +49,32 @@ pub fn get_hsl(string: &str) -> Option<Hsl> {
         None => 1.0,
     };
     Some(Hsl { h, s, l, a })
+}
+
+#[wasm_bindgen]
+pub fn get_hwb(string: &str) -> Option<Hwb> {
+    let re = Regex::new(r"^hwb\(\s*([+-]?\d{0,3}(?:\.\d+)?)(?:deg)?\s*,?\s*([+-]?[\d\.]+)%\s*,?\s*([+-]?[\d\.]+)%\s*(?:[,|/]\s*([+-]?[\d\.]+)\s*)?\)$").unwrap();
+    let captures = re.captures(string)?;
+    let h: f32 = match captures.get(1)?.as_str().parse::<f32>() {
+        Ok(num) => num.rem_euclid(360.0),
+        Err(_e) => return None,
+    };
+    let w: f32 = match captures.get(2)?.as_str().parse::<f32>() {
+        Ok(num) => clamp(num, 0.0, 100.0),
+        Err(_e) => return None,
+    };
+    let b: f32 = match captures.get(3)?.as_str().parse::<f32>() {
+        Ok(num) => clamp(num, 0.0, 100.0),
+        Err(_e) => return None,
+    };
+    let a: f32 = match captures.get(4) {
+        Some(value) => match value.as_str().parse::<f32>() {
+            Ok(num) => clamp(num, 0.0, 1.0),
+            Err(_e) => return None,
+        },
+        None => 1.0,
+    };
+    Some(Hwb { h, w, b, a })
 }
 
 fn clamp(num: f32, min: f32, max: f32) -> f32 {
--- tests/node.rs
+++ tests/node.rs
@@ -14,15 +14,6 @@ fn hsl_basic() {
     });
     assert_eq!(expected, get_hsl("hsl(240, 100%, 50.5%)"));
     assert_eq!(expected, get_hsl("hsl(240 100% 50.5%)"));
-}
-#[wasm_bindgen_test]
-fn hsl_deg() {
-    let expected = Some(Hsl {
-        h: 240.0,
-        s: 100.0,
-        l: 50.5,
-        a: 1.0,
-    });
     assert_eq!(expected, get_hsl("hsl(240deg, 100%, 50.5%)"));
     assert_eq!(expected, get_hsl("hsl(240deg 100% 50.5%)"));
 }
@@ -42,7 +33,7 @@ fn hsl_zero() {
 #[wasm_bindgen_test]
 fn hsl_range() {
     let expected = Some(Hsl {
-        h: 360.0,
+        h: 40.0,
         s: 10.0,
         l: 100.0,
         a: 1.0,
@@ -66,7 +57,7 @@ fn hsl_operator_pos() {
 #[wasm_bindgen_test]
 fn hsl_operator_neg() {
     let expected = Some(Hsl {
-        h: 0.0,
+        h: 120.0,
         s: 0.0,
         l: 0.0,
         a: 1.0,
@@ -102,7 +93,7 @@ fn hsl_alpha_zero() {
 #[wasm_bindgen_test]
 fn hsl_alpha_range() {
     let expected = Some(Hsl {
-        h: 360.0,
+        h: 40.0,
         s: 10.0,
         l: 100.0,
         a: 1.0,
@@ -126,7 +117,7 @@ fn hsl_alpha_pos() {
 #[wasm_bindgen_test]
 fn hsl_alpha_neg() {
     let expected = Some(Hsl {
-        h: 0.0,
+        h: 120.0,
         s: 0.0,
         l: 0.0,
         a: 0.0,
@@ -165,4 +156,158 @@ fn hsl_extra_at_end() {
     assert_eq!(None, get_hsl("hsl(41, 50%, 45%)1234"));
     assert_eq!(None, get_hsl("hsl(41 50% 45%)1234"));
     assert_eq!(None, get_hsl("hsl(41 50% 45% / 3)1234"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_basic() {
+    let expected = Some(Hwb {
+        h: 240.0,
+        w: 100.0,
+        b: 50.5,
+        a: 1.0,
+    });
+    assert_eq!(expected, get_hwb("hwb(240, 100%, 50.5%)"));
+    assert_eq!(expected, get_hwb("hwb(240 100% 50.5%)"));
+    assert_eq!(expected, get_hwb("hwb(240deg, 100%, 50.5%)"));
+    assert_eq!(expected, get_hwb("hwb(240deg 100% 50.5%)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_zero() {
+    let expected = Some(Hwb {
+        h: 0.0,
+        w: 0.0,
+        b: 0.0,
+        a: 1.0,
+    });
+    assert_eq!(expected, get_hwb("hwb(0, 0%, 0%)"));
+    assert_eq!(expected, get_hwb("hwb(0 0% 0%)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_range() {
+    let expected = Some(Hwb {
+        h: 40.0,
+        w: 10.0,
+        b: 100.0,
+        a: 1.0,
+    });
+    assert_eq!(expected, get_hwb("hwb(400, 10%, 200%)"));
+    assert_eq!(expected, get_hwb("hwb(400 10% 200%)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_operator_pos() {
+    let expected = Some(Hwb {
+        h: 240.0,
+        w: 100.0,
+        b: 50.5,
+        a: 1.0,
+    });
+    assert_eq!(expected, get_hwb("hwb(+240, +100%, +50.5%)"));
+    assert_eq!(expected, get_hwb("hwb(+240 +100% +50.5%)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_operator_neg() {
+    let expected = Some(Hwb {
+        h: 120.0,
+        w: 0.0,
+        b: 0.0,
+        a: 1.0,
+    });
+    assert_eq!(expected, get_hwb("hwb(-240, -100%, -50.5%)"));
+    assert_eq!(expected, get_hwb("hwb(-240 -100% -50.5%)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_alpha() {
+    let expected = Some(Hwb {
+        h: 240.0,
+        w: 100.0,
+        b: 50.5,
+        a: 0.2,
+    });
+    assert_eq!(expected, get_hwb("hwb(240, 100%, 50.5%, 0.2)"));
+    assert_eq!(expected, get_hwb("hwb(240 100% 50.5% / 0.2)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_alpha_zero() {
+    let expected = Some(Hwb {
+        h: 0.0,
+        w: 0.0,
+        b: 0.0,
+        a: 0.0,
+    });
+    assert_eq!(expected, get_hwb("hwb(0, 0%, 0%, 0)"));
+    assert_eq!(expected, get_hwb("hwb(0 0% 0% / 0)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_alpha_range() {
+    let expected = Some(Hwb {
+        h: 40.0,
+        w: 10.0,
+        b: 100.0,
+        a: 1.0,
+    });
+    assert_eq!(expected, get_hwb("hwb(400, 10%, 200%, 10)"));
+    assert_eq!(expected, get_hwb("hwb(400 10% 200% / 10)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_alpha_pos() {
+    let expected = Some(Hwb {
+        h: 240.0,
+        w: 100.0,
+        b: 50.5,
+        a: 0.8,
+    });
+    assert_eq!(expected, get_hwb("hwb(+240, +100%, +50.5%, +0.8)"));
+    assert_eq!(expected, get_hwb("hwb(+240 +100% +50.5% / +0.8)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_alpha_neg() {
+    let expected = Some(Hwb {
+        h: 120.0,
+        w: 0.0,
+        b: 0.0,
+        a: 0.0,
+    });
+    assert_eq!(expected, get_hwb("hwb(-240, -100%, -50.5%, -1)"));
+    assert_eq!(expected, get_hwb("hwb(-240 -100% -50.5% / -1)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_combined() {
+    let expected = Some(Hwb {
+        h: 240.0,
+        w: 100.0,
+        b: 50.5,
+        a: 0.0,
+    });
+    assert_eq!(expected, get_hwb("hwb(+240deg, 100%, 50.5%, -0.2)"));
+    assert_eq!(expected, get_hwb("hwb(+240deg 100% 50.5% / -0.2)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_alpha_percent() {
+    assert_eq!(None, get_hwb("hwb(250, 100%, 50%, 50%)"));
+    assert_eq!(None, get_hwb("hwb(250 100% 50% / 50%)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_extra_at_start() {
+    assert_eq!(None, get_hwb("1234hwb(41, 50%, 45%)"));
+    assert_eq!(None, get_hwb("1234hwb(41 50% 45%)"));
+    assert_eq!(None, get_hwb("1234hwb(41 50% 45% / 3)"));
+}
+
+#[wasm_bindgen_test]
+fn hwb_extra_at_end() {
+    assert_eq!(None, get_hwb("hwb(41, 50%, 45%)1234"));
+    assert_eq!(None, get_hwb("hwb(41 50% 45%)1234"));
+    assert_eq!(None, get_hwb("hwb(41 50% 45% / 3)1234"));
 }
